f6c38c3d30f4fdf65dab3f0fd8f9be05e35da9bd
EdmilsonFilho <edmilsonfof@gmail.com>
2020-11-23 17:05:13 -0300

    Exercicio em andamento

---
+++ pokedex/src/Screens/card.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { useTheme } from '@material-ui/core/styles';
+
+function Card() {
+  const theme = useTheme();
+
+  return (
+     <div>
+         funciona details
+     </div>
+  )
+}
+
+export default Card;
--- pokedex/src/componentes/router.js
+++ pokedex/src/componentes/router.js
@@ -3,25 +3,44 @@ import Details from "../Screens/details"
 import Pokedex from "../Screens/pokedex"
 import ErrorPage from "../Screens/errorPage"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+  },
+});
+
+theme = responsiveFontSizes(theme);
 
 function Router() {
-    return (
-<BrowserRouter>
+  return (
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
       <Switch>
         <Route exact path="/">
-        <HomePage />
+          <HomePage />
         </Route>
         <Route exact path="/pokedex">
-         <Pokedex/>
+          <Pokedex/>
         </Route>
         <Route exact path="/details">
-       <Details/>
+          <Details/>
         </Route>
         <Route>
-        <ErrorPage/>
+          <ErrorPage/>
         </Route>
       </Switch>
+    </ThemeProvider>
     </BrowserRouter>
- )
+  )
 }
 export default Router;
