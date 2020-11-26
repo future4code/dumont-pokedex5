e72f0d44a5a5e0c01724b75bdcae621412037672
rafae <rafaelgf@gmail.com>
2020-11-26 13:03:52 -0300

    renderização de lista de cards

--- pokedex/src/Screens/home.js
+++ pokedex/src/Screens/home.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect, useContext} from "react"
 import Header from '../componentes/Header/Header'
 import GlobalStateContext from "../global/globalStateContext"
 import axios from "axios"
+import CardComponent from '../componentes/cardComponent'
 
 
 function Home() {
@@ -11,15 +12,21 @@ function Home() {
     axios.get("https://pokeapi.co/api/v2/pokemon/").then(response =>{
       setters.setListaPokemon(response)
     })
-     },[])
+  },[])
 
   console.log(states)
 
- 
   return (
      <div>
-       <Header/>
-         
+      <Header/>
+      {states.listaPokemon !== {} ?
+        states.listaPokemon.data.results.map((pokemon)=> {
+          return (
+            <CardComponent key={pokemon.name} name={pokemon.name}/>
+          )
+        })
+      : ""
+      }
      </div>
   )
 }
